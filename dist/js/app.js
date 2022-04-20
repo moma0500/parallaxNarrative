ec3daed5aca5cc94d2ca572ee4b6367126d91ae4
McClain Martensen <moma0500@colorado.edu>
2022-04-20 00:27:29 -0600

    damn

---
+++ dist/js/app.js
@@ -0,0 +1,12 @@
+const translate = document.querySelectorAll(".translate");
+
+window.addEventListener('scroll', () => {
+  let scroll = window.pageYOffset;
+  // console.log(scroll);
+
+  translate.forEach(element => {
+    let speed = element.dataset.speed;
+    element.style.transform = 'translateY(${scroll * speed}px)';
+
+  })
+})
